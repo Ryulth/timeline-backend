12d06e64b0120200bedcddd7070d429ac9b69df7
Ryulth <adboy94@khu.ac.kr>
2019-07-15 02:01:20 +0900

    [#1]Make-initial-project
    [#1]회원가입 API, 이메일 중복체크 API 구현, 토큰 발행 후 반환

---
+++ src/main/java/com/ryulth/timeline/TimelineApplication.java
@@ -0,0 +1,11 @@
+package com.ryulth.timeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelineApplication {
+    public static void main(String args[]) {
+        SpringApplication.run(TimelineApplication.class, args);
+    }
+}
---
+++ src/main/java/com/ryulth/timeline/account/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.ryulth.timeline.account.repository;
+
+import com.ryulth.timeline.account.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
+}
